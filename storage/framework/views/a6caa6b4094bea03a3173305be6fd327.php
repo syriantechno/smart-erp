660432b3f4f8704357791dea8ebd71337754fb71
syriantechno <loayalshaeer@gmailcom>
2025-11-27 22:01:02 +0400

    remove compiled Blade view cache files from storage/framework/views directory

--- storage/framework/views/a6caa6b4094bea03a3173305be6fd327.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class(['p-2 font-medium'])->merge($attributes->whereDoesntStartWith('class')->getAttributes())); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH E:\ERP System\Source\resources\views/components/base/menu/header.blade.php ENDPATH**/ ?>
